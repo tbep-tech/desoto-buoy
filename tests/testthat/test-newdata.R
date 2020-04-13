2cf1701d5560ee3c27fd115eaa0a607027e5af70
fawda123 <mbafs2012@gmail.com>
2020-04-13 09:34:14 -0400

    fixing log read for index, test of data are new

---
+++ tests/testthat/test-newdata.R
@@ -0,0 +1,12 @@
+test_that("Checking new data, will break if data are added", {
+  
+  library(here)
+  library(dplyr)
+  
+  load(file = here('data/dat.RData'))
+  result <- nrow(dat)
+  
+  expect_equal(result, 52964)
+  
+})
+
